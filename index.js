20bdbb8020210149dacdda055b75f58ccc37b506
Scott <scotthenry1@gmail.com>
2023-01-31 12:31:28 -0500

    Completed lab assignment

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideLength){
+  if (rideLength <= 400)  {
+    return "This one is on me!";
+  }
+  else if (rideLength > 400 && rideLength <= 2000) {
+    return "That will be twenty bucks.";
+  }
+  else if (rideLength > 2000 && rideLength <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  }
+  else if (rideLength > 2500) {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  const returnMessage = city === "NYC" ? "Ok, sounds good." : "No go.";
+  return returnMessage;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(generosityLevel){
+  let returnMessage;
+  switch(generosityLevel){
+    case 'generous':
+      returnMessage = "Thank you so much.";
+      break;
+    case 'not as generous':
+      returnMessage = "Thank you.";
+      break;
+    default:
+      returnMessage = "Bye.";
+      break;
+  }
+  return returnMessage;
 }
